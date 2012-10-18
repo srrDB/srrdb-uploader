842db1f2340cdeee5f590cc226f8839dd56bf173
Gfy <gfy@lavabit.com>
2012-10-18 18:06:23 +0200

    cli argument added

--- srrdb_setup.py
+++ srrdb_setup.py
@@ -1,11 +1,15 @@
 from distutils.core import setup
 import py2exe
+import sys
 import srrdb
+
+sys.argv.append('py2exe')
 
 setup(
     # The first three parameters are not required, if at least a
     # 'version' is given, then a versioninfo resource is built from
     # them and added to the executables.
+    options = {'py2exe': {'bundle_files': 1}},
     version = srrdb.__version__,
     description = srrdb._USER_AGENT,
     name = "srrdb",
