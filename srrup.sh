f1882ffa4034152f0f8dc331dd2058857d4e6d7c
Gfy <pyrescene@gmail.com>
2015-09-24 00:56:17 +0200

    List of required changes added for old script

--- srrup.sh
+++ srrup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # Just change the username and password variables and launch with ./srrup.sh <srr file> 
 
+# Changes necessary to make this script up to date:
+# I just changed http://www.srrdb.com/login.php to http://www.srrdb.com/account/login, 
+# and removed the .php from http://www.srrdb.com/upload.php
+# I also removed the ! from login=Login! but not sure if that was necessary
+# the reasons in the script might needed updating too. 
+# I think it now says something like "is a dupe and will be checked by an administrator"
+# I just had mine dump out the entire html and check it manually :)
+
 username="sha0lin"
 password=""
 
